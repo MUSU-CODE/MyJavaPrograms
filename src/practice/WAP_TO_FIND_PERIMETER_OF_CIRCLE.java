77b3cc6b08a975c737191cd11c3e9a0339b43898
TRIPATHY <mousumi.tripathy@capgemini.com>
2025-11-21 17:52:32 +0530

    WAP_TO_FIND_PERIMETER_OF_CIRCLE

---
+++ src/practice/WAP_TO_FIND_PERIMETER_OF_CIRCLE.java
@@ -0,0 +1,11 @@
+package practice;
+
+import java.util.Scanner;
+
+public class WAP_TO_FIND_PERIMETER_OF_CIRCLE {
+
+	public static void main(String[] args) {
+		Scanner scn=new Scanner(System.in);
+	}
+
+}
