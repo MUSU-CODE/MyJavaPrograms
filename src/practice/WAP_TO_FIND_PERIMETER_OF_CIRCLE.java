88bbaf4c470ead83da85eb29b140d4f1c7dff3d4
TRIPATHY <mousumi.tripathy@capgemini.com>
2025-11-27 13:53:42 +0530

    thank you

--- src/practice/WAP_TO_FIND_PERIMETER_OF_CIRCLE.java
+++ src/practice/WAP_TO_FIND_PERIMETER_OF_CIRCLE.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 public class WAP_TO_FIND_PERIMETER_OF_CIRCLE {
 
 	public static void main(String[] args) {
-		Scanner scn=new Scanner(System.in);
-		int num=scn.nextInt();
+		Scanner scn=new Scanner(System.in); //Take input from user
+		System.out.println("Please enter radius:");
+		int radius=scn.nextInt();//whtever user enter we put it in this"radius"variable
+		scn.close();//after scanner class propose is done we need to close the class
+		
+		double perimeter=2*Math.PI*radius;//double used bcoz pi is decimal value
+		//in java there a predefined Math class there where PI is a predefined constant
+		System.out.println("Circle perimeter is "+perimeter);
 	}
 
 }
